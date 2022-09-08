88450ab2f1e7b32802494c8ca63894c463a97844
Dixy Prevner M'POUTOU BABINGUI <dixy-prevner.m-poutou-babingui@estiam.com>
2022-09-08 18:30:16 +0200

    mise à jour du tableau

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/products.model';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -7,38 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  products!: Array<any>;
-  constructor() { }
+  products!: Array<Product>;
+  errorMessage ! : string;
+
+  //nous allons utiliser notre service dans le constructeur de notre constructeur de composant
+  constructor(private productService : ProductService) {}
 
   // s'exécute au démarrage
   ngOnInit(): void {
-    this.products =  [
-      {
-        id : 1,
-        name: "computer",
-        price: 6500
-      },
-      {
-        id : 2,
-        name: "Printer",
-        price: 1200
-      },
-      {
-        id : 3,
-        name: "Smart phone",
-        price: 1400
-      }
-    ];
+     
+    this.handleGetAllProducts();
+   
+  }
 
+  //je crée ma méthode qui va me permettre de récuperer tous les produits
+  handleGetAllProducts()
+  {
+    this.productService.getAllProducts().subscribe(
+      {
+        //si tout se passe bien j'ai mon next au cas contraire j'ai une erreur
+        next : (data) =>{
+          this.products = data;
+        },
+        error : (err)=>{
+              this.errorMessage = err;
+        }
+      }
+     );
   }
 
   //création de ma méthode me permettant de supprimer un produit
-  handleDeleteProduct(p : any)
+  handleDeleteProduct(p : Product)
   {
-    //je recherche l'index de mon projet
-    let index = this.products.indexOf(p)
-    this.products.splice(index,1)
+       let conf = confirm("Are you sure?")
+       
 
+       if(conf==false) return;
+        //console.log(p.id);
+        this.productService.deleteProduct(p.id).subscribe({
+          next : (data:boolean)=>{
+            let index = this.products.indexOf(p)
+            this.products.splice(index)
+          }
+        });
+        
+  }
+
+  handleSetPromotion(p : Product)
+  {
+    let promo = p.promotion
+    this.productService.setPromotion(p.id).subscribe(
+      {
+        next: (data : boolean)=>{
+          p.promotion = !promo
+
+        },
+        error : err =>{
+          this.errorMessage = err;
+        }
+      }
+    )
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { Product } from '../model/products.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' //disponible dans la racine du projet, tout le projet
 })
 export class ProductService {
 
-  constructor() { }
+  private products! : Array<Product>;
+
+  constructor() { 
+    this.products =  [
+      {
+        id : 1,
+        name: "computer",
+        price: 6500,
+        promotion : true
+      },
+      {
+        id : 2,
+        name: "Printer",
+        price: 1200,
+        promotion : false
+      },
+      {
+        id : 3,
+        name: "Smart phone",
+        price: 1400,
+        promotion : true
+      }
+    ];
+
+  }
+
+  //methode permettant de retourner tous les données
+
+  public getAllProducts() : Observable<Array<Product>>{
+    let rnd =Math.random()
+    if(rnd<0.1) return throwError(()=>new Error("internet  connection error"))
+    else
+    return of(this.products)
+  }
+
+  //methode permettant de supprimer une donnée
+  public deleteProduct(id : number) : Observable<boolean>
+  {
+    this.products = this.products.filter(p=>p.id!=id)
+    return of(true)
+  }
+
+  //méthode permettant de modifier une promotion
+  public setPromotion(id : number) : Observable<boolean>
+  {
+    let product =  this.products.find(p=>p.id==id);
+    if (product !=  undefined) {
+        product.promotion = !product.promotion
+
+        return of(true)
+    }else return throwError(()=> new Error("Product not found"))
+  }
 }
